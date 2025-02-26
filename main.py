4381614fa38986e5b6394aa3d81aeb742429852d
Diego Vasconcelos <diegovasconcelosb@gmail.com>
2025-02-26 15:56:31 -0300

    feat: add lifespan to configure dependencies

--- main.py
+++ main.py
@@ -1,24 +1,46 @@
+"""
+This module initializes the FastAPI application.
+"""
+import logging
+from contextlib import asynccontextmanager
+import httpx
 from fastapi import FastAPI
 from routes.order_router import router
-import logging
+from clients.customer_client import CustomerClient
+from clients.product_client import ProductClient
+from configs.db_conn import get_database
+from services.order_service import OrderService
+from storage.order_storage import OrderStorage
+
+logger = logging.getLogger(__name__)
+logging.getLogger("pymongo").setLevel(logging.WARNING)
 
 
-app = FastAPI()
-logging.basicConfig(
-        level=logging.DEBUG,
-        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
-        handlers=[
-            logging.StreamHandler(),
-        ],
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    """
+    Manage the application's lifespan, setting up dependencies.
+    """
+    db_connection = get_database()
+    order_storage = OrderStorage(db_connection)
+    customer_client = CustomerClient(httpx)
+    product_client = ProductClient(httpx)
+
+    order_service = OrderService(
+        order_storage, customer_client, product_client
     )
+
+    yield {"order_service": order_service}
+    logger.info("Shutdown Application")
+
+
+app = FastAPI(lifespan=lifespan, title="Order Service")
+app.include_router(router)
 
 
 @app.get("/health")
 def health_check():
     """
-    Healthy check to see if the application is working in a basic way
+    Health check endpoint to verify if the application is running.
     """
     return {"status": "healthy"}
-
-
-app.include_router(router)
