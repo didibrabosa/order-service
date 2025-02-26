9fdb8971eb74f7e734333c3771d0c9c44383ef67
Diego Vasconcelos <diegovasconcelosb@gmail.com>
2025-02-26 15:53:49 -0300

    refactor: enhance object attributes with Pydantic and add docstrings

--- models/order_model.py
+++ models/order_model.py
@@ -1,40 +1,67 @@
-from pydantic import BaseModel, EmailStr, Field
-import ulid
+"""
+This layer deals with models, specifying what each model needs and uses.
+"""
 from typing import List
 from datetime import datetime
+import ulid
+from pydantic import BaseModel, EmailStr, Field
 
 
 class Product(BaseModel):
-    id: str = Field(default_factory=lambda: str(ulid.new()))
-    name: str
-    description: str
-    price: float
-    quantity: int
+    """
+    Represents a product with its details.
+    """
+    id: str = Field(
+        default_factory=lambda: str(ulid.new()), description="Product ID")
+    name: str = Field(description="Product Name")
+    description: str = Field(description="Product Description")
+    price: float = Field(gt=0, description="Product Price")
+    quantity: int = Field(description="Product Quantity")
 
 
 class Customer(BaseModel):
-    id: str = Field(default_factory=lambda: str(ulid.new()))
-    name: str
-    email: EmailStr
+    """
+    Represents a customer and their contact details.
+    """
+    id: str = Field(
+        default_factory=lambda: str(ulid.new()), description="Customer ID")
+    name: str = Field(description="Customer Name")
+    email: EmailStr = Field(description="Customer Email")
 
 
 class Order(BaseModel):
-    id: str = Field(default_factory=lambda: str(ulid.new()))
+    """
+    Represents an order placed by a customer.
+    """
+    id: str = Field(
+        default_factory=lambda: str(ulid.new()), description="Order ID")
     customer: Customer
     products: List[Product]
-    created_at: datetime = Field(default_factory=datetime.now)
-    updated_at: datetime = Field(default=None)
+    created_at: datetime = Field(
+        default_factory=datetime.now, description="Create Order Timestamp")
+    updated_at: datetime | None = Field(
+        default=None, description="Update Order Timestamp")
 
 
 class ProductRequest(BaseModel):
-    name: str
-    quantity: int
+    """
+    Represents a request to add or update a product.
+    """
+    name: str = Field(description="Product Name")
+    quantity: int = Field(description="Product Quantity")
 
 
 class OrderRequest(BaseModel):
-    email: EmailStr
+    """
+    Represents a request to create a new order.
+    """
+    email: EmailStr = Field(description="Customer Email")
     products: List[ProductRequest]
 
 
 class OrderResponse(BaseModel):
-    id: str = Field(default_factory=lambda: str(ulid.new()))
+    """
+    Represents the response for an order operation.
+    """
+    id: str = Field(
+        default_factory=lambda: str(ulid.new()), description="Order ID")
