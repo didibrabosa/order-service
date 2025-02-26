d373fc006798e17686d894380374f1f49f31de18
Diego Vasconcelos <diegovasconcelosb@gmail.com>
2025-02-26 15:49:52 -0300

    refactor: improve docstrings, dependency injection, and better object orientation

--- routes/order_router.py
+++ routes/order_router.py
@@ -1,28 +1,41 @@
-from fastapi import APIRouter, HTTPException
-from services.order_service import OrderService
-from storage.order_storage import OrderStorage
-from clients.customer_client import CustomerClient
-from clients.product_client import ProductClient
-from models.order_model import OrderRequest, OrderResponse
-from configs.db_conn import get_database
+"""
+This module defines the routes for managing orders in the Order Service API.
+"""
 import logging
+from typing import Annotated
+import httpx
+from fastapi import APIRouter, HTTPException, Depends, Request
+from models.order_model import OrderRequest, OrderResponse
+from services.order_service import OrderService
 
 router = APIRouter()
-logger = logging.getLogger("__name__")
+logger = logging.getLogger(__name__)
 
-order_storage = OrderStorage(db_connection=get_database())
-customer_client = CustomerClient()
-product_client = ProductClient()
-service = OrderService()
+
+def get_order_service(request: Request) -> OrderService:
+    """
+    Retrieve the OrderService instance from the request's state.
+    """
+    return request.state.order_service
+
+
+ServiceDep = Annotated[OrderService, Depends(get_order_service)]
 
 
 @router.post("/v1/orders", response_model=OrderResponse)
-def create_order(order: OrderRequest):
-    logger.debug(f"Order payload: {order}")
+def create_order(order: OrderRequest, service: ServiceDep) -> OrderResponse:
+    """
+    Handle the creation of a new order.
+    """
     try:
-        logger.info("Creating order %s...", order)
+        logger.info("Starting to create an order.")
         created_order = service.create_order(order)
+
         logger.info("Order created successfully: %s", created_order)
-        return {"message": "Order created successfully", "order": created_order}
-    except ValueError as ex:
-        raise HTTPException(status_code=500, detail=f"Error to create order: {ex}")
+        return OrderResponse(created_order)
+
+    except httpx.HTTPStatusError as ex:
+        logger.error("Error creating order: %s", ex)
+        raise HTTPException(
+            status_code=500, detail=f"Error creating order: {ex}"
+        ) from ex
